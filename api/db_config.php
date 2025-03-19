01867c43bf258cd219fd8cf1d66fccf7875cebfa
Matt Cabarrubias <monapwc2020@gmail.com>
2025-03-19 09:23:56 +0800

    CONFIG: wrong config

--- api/db_config.php
+++ api/db_config.php
@@ -1,5 +1,5 @@
 <?php 
-    define("DB_HOST", "localhost:3307");
+    define("DB_HOST", "localhost:3306");
     define("DB_USER", "s22103604_chiase");
     define("DB_PASS", "sonicdravice");
     define("DB_NAME", "s22103604_chiase");
